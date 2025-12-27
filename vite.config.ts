461e3ee6de108b5d52de2ef29678270a94d6cb5f
parultandon1999 <parul.tandon.1999@gmail.com>
2025-12-27 16:20:44 +0530

    Fix base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/parul_portfolio/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
